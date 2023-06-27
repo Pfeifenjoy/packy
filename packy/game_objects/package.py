d04419c7cd52168da757db892e57918218aed41f
Arwed Mett <arwed.mett@googlemail.com>
2023-06-27 23:58:48 +0200

    gamplay, speed and spawn behaviour

--- packy/game_objects/package.py
+++ packy/game_objects/package.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from typing import Callable
 from pygame import Surface, Color
+from pygame.font import SysFont, get_default_font
 
 from packy.game_object import GameObject
 from packy.context import Context
@@ -10,13 +10,13 @@ from packy.shapes import Box
 from packy.update import Update
 
 from .rectangle import Rectangle
+from .text import Text
 
 
 class Package(GameObject):
-
+    number: int
     position: RelativeVector
     dimensions: RelativeVector
-    on_destroy: Callable[[Package], None]
 
     velocity: int
     direction: RelativeVector
@@ -24,19 +24,19 @@ class Package(GameObject):
     def __init__(
             self: Package,
             context: Context,
+            number: int,
             position: RelativeVector,
             width: int,
             velocity: int,
-            on_destroy: Callable[[Package], None]
     ) -> None:
         super().__init__(context)
+        self.number = number
 
         self.position = position
         self.dimensions = RelativeVector(
             width, int(self.context.coordinate_system.get_ratio() * width)
         )
         self.velocity = velocity
-        self.on_destroy = on_destroy
         self.direction = RelativeVector(0, 1)
 
     def get_motion(self: Package, update: Update) -> RelativeVector:
@@ -46,9 +46,6 @@ class Package(GameObject):
 
     def update(self: Package, update: Update) -> None:
         self.position = self.position.add(self.get_motion(update))
-
-        if self.position.get_y() > 1000000:
-            self.on_destroy(self)
 
     def get_box(self: Package) -> Box:
         return Box(self.position, self.dimensions)
@@ -61,3 +58,13 @@ class Package(GameObject):
             outline=Color(115, 83, 62)
         )
         rectangle.draw(canvas)
+        text = Text(
+            self.context,
+            self.get_box().center(),
+            str(self.number),
+            SysFont(get_default_font(), 20)
+        )
+        text.draw(canvas)
+
+    def __hash__(self: Package) -> int:
+        return self.number
--- packy/game_objects/package_mount.py
+++ packy/game_objects/package_mount.py
@@ -28,7 +28,10 @@ class PackageMount(GameObject):
     package_height: int
 
     cursor: int
-    velocity: int
+    velocity = 50000  # cords / sec
+    spawn_rate = 7
+
+    spawned_packages = 0
 
     def __init__(self: PackageMount, context: Context, game_state: GameState) -> None:
         super().__init__(context)
@@ -40,7 +43,6 @@ class PackageMount(GameObject):
         self.package_height = int(self.package_width * self.context.coordinate_system.get_ratio())
 
         self.cursor = 0
-        self.velocity = 50000  # cords / sec
 
     def destroy_package(self: PackageMount, package: Package) -> None:
         logger.info("Destroy package.")
@@ -49,7 +51,7 @@ class PackageMount(GameObject):
 
     def generate_package(self: PackageMount) -> Package:
         logger.info("Generate package.")
-        location = randint(0, self.positions)
+        location = randint(0, self.positions - 1)
         position = RelativeVector(
             int(location * self.package_width),
             int(-1 * self.package_height)
@@ -57,13 +59,19 @@ class PackageMount(GameObject):
 
         package = Package(
             self.context,
+            self.spawned_packages,
             position,
             self.package_width,
-            self.velocity,
-            self.destroy_package
+            self.velocity
         )
 
         self.packages.add(package)
+        self.spawned_packages += 1
+
+        self.velocity = min(200000, self.velocity + 1000)
+
+        if self.spawned_packages % 20 == 0:
+            self.spawn_rate = max(self.spawn_rate - 1, 3)
 
         return package
 
@@ -71,14 +79,22 @@ class PackageMount(GameObject):
         # generate new packages
         self.cursor += int(self.velocity * (update.elapsed_time.microseconds / 1000000))
 
-        if self.cursor >= self.package_height:
+        if self.cursor >= self.package_height * self.spawn_rate:
             self.generate_package()
 
             self.cursor = 0
 
         # update packages
-        for package in set(self.packages):
+        for package in self.packages:
             package.update(update)
+
+        destroyable_packages = [
+            package for package in self.packages
+            if package.position.get_y() > 1000000
+        ]
+
+        for package in destroyable_packages:
+            self.destroy_package(package)
 
     def draw(self: PackageMount, canvas: Surface) -> None:
         for package in self.packages:
